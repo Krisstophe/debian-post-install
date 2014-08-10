88a21007eb6d70e2ae8ec753fcc64ef366bae02c
jjzz <freeman_cha@hotmail.com>
2014-08-10 16:24:53 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -6,24 +6,30 @@ themv=`which mv`
 ######################
 # update & install
 
-# apt-get -y update
-# apt-get -y upgrade
-# apt-get -y dist-upgrade
-# apt-get -y install vim ctags cscope ascii vim lynx pptpd tmux p7zip-full tree netselect emacs gawk lynx nmap
-# apt-get -y install build-essential autoconf automake gdb dump strace libtool
-# apt-get -y install python3 python3-pip python-pip
-# apt-get -y install binutils-doc vim-doc
-# apt-get -y upgrade
-# apt-get -y dist-upgrade
+apt-get -y update
+apt-get -y upgrade
+apt-get -y dist-upgrade
+apt-get -y install vim ctags cscope ascii vim lynx pptpd tmux p7zip-full tree netselect emacs gawk lynx nmap
+apt-get -y install build-essential autoconf automake gdb dump strace libtool
+apt-get -y install python3 python3-pip python-pip
+apt-get -y install binutils-doc vim-doc
+apt-get -y upgrade
+apt-get -y dist-upgrade
 
 ######################
 # modify configuration
+
+# update pptpd password to .sed rule
+echo input the password for pptpd service:
+read pptpdpwd
+sed "s/\(.*\)<changepassword>\(.*\)/\1$pptpdpwd\2/" ./sed/chap-secrets.sed > ./sed/chap-secrets.sed2
+$themv ./sed/chap-secrets.sed2 ./sed/chap-secrets.sed
 
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables-save > /etc/iptables-rules
 ip6tables-save > /etc/ip6tables-rules
 
-# all conf needed to be modified
+# conf files list
 configfiles=(
 /etc/ssh/sshd_config         # modify ssh port
 /etc/sysctl.conf
@@ -36,21 +42,16 @@ configfiles=(
 for conffile in ${configfiles[@]}; do
     $themv -f $conffile $conffile.bak
     fileshortname=`expr $conffile : '.*\/\(.*\)'` # 获得短文件名
-    sed -f ./sed/$fileshortname.sed $configfile.bak > $configfile
+    sed -f ./sed/$fileshortname.sed $conffile.bak > $confile
 done
 
-# the pptpd password
-echo input the password for pptpd service:
-read pptpdpwd
-sed "s/\(.*\)<changepassword>\(.*\)/\1$pptpdpwd\2/" ./sed/chap-secrets.sed > ./sed/chap-secrets.sed2
-$themv ./sed/chap-secrets.sed2 ./sed/chap-secrets.sed
-
 sysctl -p
-service sshd restart
+service ssh restart
 service pptpd restart
 
 ######################
 # copy initial environment files
+
 $thecp -r -f .bashalias .gitconfig .pythonstartup .tmux.conf .toprc .vimrc .wgetrc .vim bin -t ~/
 
 # import ssh public key
